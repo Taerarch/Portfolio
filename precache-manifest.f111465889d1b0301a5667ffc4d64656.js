665fa2c41a8dd831652db1e9ace50f5826fb93d6
Matthew <matthew.tudman@gmail.com>
2020-08-13 16:04:18 +1000

    Updates

---
+++ precache-manifest.f111465889d1b0301a5667ffc4d64656.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c2f67cddd7ee5c769becbfbd76b53cf",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "c4253ee8912fbbd6ebd6",
+    "url": "/Portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "dc9d8d16c180e4f98925",
+    "url": "/Portfolio/static/css/main.e003f981.chunk.css"
+  },
+  {
+    "revision": "c4253ee8912fbbd6ebd6",
+    "url": "/Portfolio/static/js/2.82840d04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.82840d04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc9d8d16c180e4f98925",
+    "url": "/Portfolio/static/js/main.ce02d100.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  }
+]);

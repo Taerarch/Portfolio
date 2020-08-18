a8db15686098bac56d60f086ef78269822e3d2d1
Matthew <matthew.tudman@gmail.com>
2020-08-18 13:47:01 +1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.9d94e5305484d13536ca4bfd9826d5d3.js"
+  "/Portfolio/precache-manifest.5286741630deed21623e7e860ac477a6.js"
 );
 
 self.addEventListener('message', (event) => {
